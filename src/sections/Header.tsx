58568498f847eef36968eadd0073bf90dc26656b
Levi <64612246+Carlos-Levi@users.noreply.github.com>
2024-08-14 17:44:59 -0300

    Update Header.tsx

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -8,10 +8,10 @@ export const Header = () => {
     <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">
-          Streamline your workflow and boost your productivity
+          Simplifique seu fluxo de trabalho e aumente sua produtividade
         </p>
         <div className="inline-flex gap-1 items-center">
-          <p>Get started for free</p>
+          <p>Experimente grátis</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
         </div>
       </div>
@@ -21,13 +21,13 @@ export const Header = () => {
             <Image src={Logo} alt="Saas Logo" height={40} width={40} />
             <MenuIcon className="h-5 w-5 md:hidden" />
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
-              <a href="#">About</a>
+              <a href="#">Sobre</a>
               <a href="#">Features</a>
-              <a href="#">Customers</a>
-              <a href="#">Updates</a>
-              <a href="#">Help</a>
+              <a href="#">Clientes</a>
+              <a href="#">Atualizações</a>
+              <a href="#">Ajuda</a>
               <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
-                Get for free
+                Teste grátis
               </button>
             </nav>
           </div>
