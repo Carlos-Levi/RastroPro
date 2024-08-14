7a53a4ea23ca1231b33e1996dadb3e9a2df6633a
Levi <64612246+Carlos-Levi@users.noreply.github.com>
2024-08-14 18:32:08 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { twMerge } from "tailwind-merge";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Rastro Pro Landing Page",
+  description: "Transforme suas ideias em resultados",
 };
 
 export default function RootLayout({
